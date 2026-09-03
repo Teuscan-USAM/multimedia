060d9d6290ad65eaf220c0cbb81eb070dd886fa0
Teuscan-USAM <henrry.canas260847@usam.edu.sv>
2026-09-03 14:44:46 -0600

    Contenedor2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'Checkrol' => \App\Http\Middleware\Checkrol::class,
         ]);
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -49,7 +49,7 @@
   <script src="{{ asset('NiceAdmin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
   <script src="{{ asset('NiceAdmin/assets/vendor/chart.js/chart.umd.js') }}"></script>
   <script src="{{ asset('NiceAdmin/assets/vendor/echarts/echarts.min.js') }}"></script>
-  <script src="{{ asset('Niceadmin/assets/vendor/quill/quill.js') }}"></script>
+  <script src="{{ asset('NiceAdmin/assets/vendor/quill/quill.js') }}"></script>
   <script src="{{ asset('NiceAdmin/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
   <script src="{{ asset('NiceAdmin/assets/vendor/tinymce/tinymce.min.js') }}"></script>
   <script src="{{ asset('NiceAdmin/assets/vendor/php-email-form/validate.js') }}"></script>
